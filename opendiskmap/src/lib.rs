eb7c771321e92103979e36e66b59697c46568be1
Fabian Murariu <murariu.fabian@gmail.com>
2025-08-14 23:27:19 +0100

    first take on heap

--- opendiskmap/src/lib.rs
+++ opendiskmap/src/lib.rs
@@ -4,6 +4,7 @@ pub mod disk_map;
 pub mod entry;
 pub mod error;
 mod fixed_buffers;
+pub mod heap;
 mod storage;
 pub mod types;
 pub use buffers::Buffers;
@@ -12,4 +13,5 @@ pub use disk_map::{
     DiskHashMap, MapEntry, OccupiedEntry, StringStringMap, StringU64Map, U64StringMap, VacantEntry,
 };
 pub use error::{DiskMapError, Result};
+pub use heap::{Heap, Index, Slab};
 pub use types::{Bytes, BytesDecode, BytesEncode, Native, Str};
